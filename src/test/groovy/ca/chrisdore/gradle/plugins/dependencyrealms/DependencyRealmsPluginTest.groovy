7248ec9f4026c07a735e4f3901b12ac786ba252d
Chris Dore <oesolutions@gmail.com>
2016-05-19 23:46:54 -0600

    Update license and copyright. Switch to using Java Gradle Plugin development plugin. Set root project's group, name, and version. Fix typo in package directory for test classes.

--- src/test/groovy/ca/chrisdore/gradle/plugins/dependencyrealms/DependencyRealmsPluginTest.groovy
+++ src/test/groovy/ca/chrisdore/gradle/plugins/dependencyrealms/DependencyRealmsPluginTest.groovy
@@ -24,14 +24,25 @@
 
 package ca.chrisdore.gradle.plugins.dependencyrealms
 
-import org.gradle.api.Plugin
 import org.gradle.api.Project
+import org.gradle.testfixtures.ProjectBuilder
+import spock.lang.Specification
 
-class DependencyRealmsPlugin implements Plugin<Project>
+class DependencyRealmsPluginTest extends Specification
 {
-    @Override
-    void apply( Project project )
+    Project project
+
+    def setup()
     {
-        project.logger.lifecycle 'applying plugin'
+        project = ProjectBuilder.builder().build()
+    }
+
+    def 'can apply plugin'()
+    {
+        when:
+        project.apply plugin: DependencyRealmsPlugin
+
+        then:
+        project.pluginManager.hasPlugin 'ca.chrisdore.dependencyrealms'
     }
 }
